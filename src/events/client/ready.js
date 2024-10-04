0cdb6916203017c41511f9fc855ed326d76bafa6
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2024-10-05 01:56:54 +0200

    Add update check

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,3 +1,5 @@
+const checkUpdates = require('../../functions/tools/checkUpdates');
+
 module.exports = {
     name: 'ready',
     once: true,
@@ -6,5 +8,16 @@ module.exports = {
         const chalk = await import('chalk');
         console.log(chalk.default.greenBright(`[Event Handler] Ready as ${client.user.tag}`));
         console.log(chalk.default.greenBright(`[Event Handler] Version: ${client.version}`));
+
+        // Check for updates
+        console.log(chalk.default.blueBright('[GitHub] Checking for updates...'));
+        const res = await checkUpdates();
+
+        if (res.isOutdated) {
+            console.warn(chalk.default.yellowBright(`[GitHub] The bot is outdated! Current version: ${client.version}, Latest version: ${res.latestVersion}`));
+            console.log(chalk.default.yellowBright(`[GitHub] Download the latest version at https://github.com/Bartolumiu/dexchan_rework/releases/latest`));
+        } else if (res.isOutdated === false) {
+            console.log(chalk.default.greenBright('[GitHub] The bot is up-to-date!'));
+        } // If res.isOutdated is null, the error message will be logged in the checkUpdates function itself, so no need to log it here
     }
 }

a57a8713c735cd716eb5228031ca8533dbce4821
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2024-10-15 17:08:18 +0200

    Refactored /ping command

--- src/commands/utils/ping.js
+++ src/commands/utils/ping.js
@@ -3,6 +3,11 @@ const translateAttribute = require('../../functions/handlers/translateAttribute'
 let version = require('../../../package.json').version;
 const https = require('https');
 
+const API_URLS = {
+    MANGADEX: 'https://api.mangadex.org/ping',
+    NAMICOMI: 'https://api.namicomi.com/ping'
+}
+
 module.exports = {
     global: true,
     data: async () => {
@@ -24,60 +29,69 @@ module.exports = {
         // Calculate the latency
         const ping = message.createdTimestamp - interaction.createdTimestamp;
 
-        const title = client.translate(locale, 'commands', 'ping.response.title');
-        const ws_ping_name = client.translate(locale, 'commands', 'ping.response.fields[0].name');
-        const ws_ping_value = client.translate(locale, 'commands', 'ping.response.fields[0].value', { ping: ping });
-        const discord_ping_name = client.translate(locale, 'commands', 'ping.response.fields[1].name');
-        const discord_ping_value = client.translate(locale, 'commands', 'ping.response.fields[1].value', { apiPing: client.ws.ping });
-        const md_ping_name = client.translate(locale, 'commands', 'ping.response.fields[2].name');
-        const nami_ping_name = client.translate(locale, 'commands', 'ping.response.fields[3].name');
-        const footer = client.translate(locale, 'commands', 'ping.response.footer', { commandName: `/${interaction.commandName}`, user: interaction.user.username });
+        const fields = {
+            title: await client.translate(locale, 'commands', 'ping.response.title'),
+            ws: {
+                name: await client.translate(locale, 'commands', 'ping.response.fields.bot_latency.name'),
+                value: await client.translate(locale, 'commands', 'ping.response.fields.bot_latency.value', { ping: ping }),
+                inline: true
+            },
+            discord: {
+                name: await client.translate(locale, 'commands', 'ping.response.fields.api.discord.name'),
+                value: await client.translate(locale, 'commands', 'ping.response.fields.api.discord.value', { apiPing: client.ws.ping }),
+                inline: true
+            },
+            md: {
+                name: await client.translate(locale, 'commands', 'ping.response.fields.api.mangadex.name'),
+                value: null,
+                inline: true
+            },
+            nami: {
+                name: await client.translate(locale, 'commands', 'ping.response.fields.api.namicomi.name'),
+                value: null,
+                inline: true
+            },
+            not_ok: await client.translate(locale, 'commands', 'ping.response.not_ok'),
+            footer: await client.translate(locale, 'commands', 'ping.response.footer', { commandName: `/${interaction.commandName}`, user: interaction.user.username })
+        };
 
         const embed = new EmbedBuilder()
-            .setTitle(title)
-            .addFields(
-                { name: ws_ping_name, value: ws_ping_value, inline: true },
-                { name: discord_ping_name, value: discord_ping_value, inline: true }
-            )
-            .setFooter({ text: footer, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
+            .setTitle(fields.title)
+            .addFields(fields.ws, fields.discord)
+            .setFooter({ text: fields.footer, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
             .setColor(Colors.Blurple)
             .setTimestamp();
 
         try {
-            const mdPing = await new Promise((resolve, reject) => {
-                const start = Date.now();
-                https.get('https://api.mangadex.org/ping', { headers: { 'User-Agent': `Dex-chan/${version} by Bartolumiu` } }, (res) => {
-                    res.on('data', () => { });
-                    res.on('end', () => {
-                        resolve(Date.now() - start);
-                    });
-                }).on('error', (e) => {
-                    reject(e);
-                });
-            });
-
-            const namiPing = await new Promise((resolve, reject) => {
-                const start = Date.now();
-                https.get('https://api.namicomi.com/ping', { headers: { 'User-Agent': `Dex-chan/${version} by Bartolumiu` } }, (res) => {
-                    res.on('data', () => { });
-                    res.on('end', () => {
-                        resolve(Date.now() - start);
-                    });
-                }).on('error', (e) => {
-                    reject(e);
-                });
-            });
-
-            const md_ping_value = client.translate(locale, 'commands', 'ping.response.fields[2].value', { mdPing: mdPing });
-            embed.addFields({ name: md_ping_name, value: md_ping_value, inline: true });
-
-            const nami_ping_value = client.translate(locale, 'commands', 'ping.response.fields[3].value', { ncPing: namiPing });
-            embed.addFields({ name: nami_ping_name, value: nami_ping_value, inline: true });
+            fields.md.value = client.translate(locale, 'commands', 'ping.response.fields.api.mangadex.value', { mdPing: await getPing(API_URLS.MANGADEX) });
+            embed.addFields(fields.md);
         } catch (e) {
-            const md_ping_value = client.translate(locale, 'commands', 'ping.response.md_not_ok');
-            embed.addFields({ name: md_ping_name, value: md_ping_value, inline: true });
-        }
+            console.error('Error while fetching Mangadex API ping:', e);
+            embed.addFields(fields.md);
+        } finally {
+            try {
+                fields.nami.value = client.translate(locale, 'commands', 'ping.response.fields.api.namicomi.value', { ncPing: await getPing(API_URLS.NAMICOMI) });
+                embed.addFields(fields.nami);
+            } catch (e) {
+                console.error('Error while fetching Namicomi API ping:', e);
+                embed.addFields({ name: fields.nami.name, value: fields.not_ok, inline: true });
+            }
+        }   
 
         await interaction.editReply({ embeds: [embed] });
     }
+};
+
+async function getPing(url) {
+    return new Promise((resolve, reject) => {
+        const start = Date.now();
+        https.get(url, { headers: { 'User-Agent': `Dex-chan/${version} by Bartolumiu` } }, (res) => {
+            res.on('data', () => { });
+            res.on('end', () => {
+                resolve(Date.now() - start);
+            });
+        }).on('error', (e) => {
+            reject(e);
+        });
+    });
 };

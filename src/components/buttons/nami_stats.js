5cbd5a91e6cff85c20ae1b8e39e0c9cc2a6c2f67
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2024-12-12 11:40:46 +0100

    Refactor NamiStats command to improve localization handling and embed structure

--- src/components/buttons/nami_stats.js
+++ src/components/buttons/nami_stats.js
@@ -5,10 +5,29 @@ module.exports = {
         customId: /^nami_stats_/,
     },
     async execute(interaction, client) {
+        await interaction.deferUpdate();
         const userSettings = await client.getMongoUserData(interaction.user);
         const locale = userSettings.preferredLocale || interaction.locale;
-        await interaction.deferUpdate();
         const customId = interaction.customId;
+        const translations = {
+            embed: {
+                title: await client.translate(locale, 'commands', 'nami.response.stats.title'),
+                description: await client.translate(locale, 'commands', 'nami.response.stats.description'),
+                fields: {
+                    rating: await client.translate(locale, 'commands', 'nami.response.stats.fields.rating.name'),
+                    views: await client.translate(locale, 'commands', 'nami.response.stats.fields.views.name'),
+                    follows: await client.translate(locale, 'commands', 'nami.response.stats.fields.follows.name'),
+                    comments: await client.translate(locale, 'commands', 'nami.response.stats.fields.comments.name'),
+                    chapterViews: await client.translate(locale, 'commands', 'nami.response.stats.fields.chapterViews.name'),
+                    chapterComments: await client.translate(locale, 'commands', 'nami.response.stats.fields.chapterComments.name'),
+                    chapterReactions: await client.translate(locale, 'commands', 'nami.response.stats.fields.chapterReactions.name')
+                },
+                units: {
+                    votes: await client.translate(locale, 'commands', 'nami.response.stats.units.votes')
+                },
+                footer: await client.translate(locale, 'commands', 'nami.response.footer', { commandName: `/${interaction.message.interaction.commandName}`, user: interaction.user.username })
+            }
+        }
 
         const match = customId.match(/^nami_stats_(.+)$/);
         if (!match) return null; // This should never happen even if NamiComi changes their ID format (famous last words, I know)
@@ -37,50 +56,47 @@ module.exports = {
 
         // Create an embed with the rating and statistics data
         const embed = new EmbedBuilder()
-            .setTitle(client.translate(locale, 'commands', 'nami.response.stats.title'))
-            .setDescription(client.translate(locale, 'commands', 'nami.response.stats.description', { titleId: title }))
+            .setTitle(translations.embed.title)
+            .setDescription(translations.embed.description.replace('%titleId%', title))
             .addFields(
                 {
-                    name: await client.translate(locale, 'commands', 'nami.response.stats.fields[0].name'),
-                    value: `${ratingData.rating.toFixed(2)}/5.00 (${ratingData.count} ${client.translate(locale, 'commands', 'nami.response.stats.units.votes')})`,
+                    name: translations.embed.fields.rating,
+                    value: `${ratingData.rating.toFixed(2)}/5.00 (${ratingData.count} ${translations.embed.units.votes})`,
                     inline: true
                 },
                 {
-                    name: await client.translate(locale, 'commands', 'nami.response.stats.fields[1].name'),
+                    name: translations.embed.fields.views,
                     value: `${statsData.viewCount}`,
                     inline: true
                 },
                 {
-                    name: await client.translate(locale, 'commands', 'nami.response.stats.fields[2].name'),
+                    name: translations.embed.fields.follows,
                     value: `${statsData.followCount}`,
                     inline: true
                 },
                 {
-                    name: await client.translate(locale, 'commands', 'nami.response.stats.fields[3].name'),
+                    name: translations.embed.fields.comments,
                     value: `${statsData.commentCount}`,
                     inline: true
                 },
                 {
-                    name: await client.translate(locale, 'commands', 'nami.response.stats.fields[4].name'),
+                    name: translations.embed.fields.chapterViews,
                     value: `${chapterViewCounts}`,
                     inline: true
                 },
                 {
-                    name: await client.translate(locale, 'commands', 'nami.response.stats.fields[5].name'),
+                    name: translations.embed.fields.chapterComments,
                     value: `${chapterCommentCounts}`,
                     inline: true
                 },
                 {
-                    name: await client.translate(locale, 'commands', 'nami.response.stats.fields[6].name'),
+                    name: translations.embed.fields.chapterReactions,
                     value: `${chapterReactionCounts}`,
                     inline: true
                 }
             )
             .setColor(Colors.Blurple)
-            .setFooter({
-                text: await client.translate(locale, 'commands', 'nami.response.footer', { commandName: `/${interaction.message.interaction.commandName}`, user: interaction.user.username }),
-                iconURL: client.user.displayAvatarURL({ dynamic: true })
-            });
+            .setFooter({ text: translations.embed.footer, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
 
         // Send a follow-up message with the embed
         await interaction.followUp({ embeds: [embed] });

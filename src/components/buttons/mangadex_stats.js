840a160c40bacf550d1f91de516c995c9c74747f
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2024-12-12 11:51:19 +0100

    Refactor mangadex_stats command to enhance localization handling and embed structure

--- src/components/buttons/mangadex_stats.js
+++ src/components/buttons/mangadex_stats.js
@@ -5,10 +5,37 @@ module.exports = {
         customId: /^mangadex_stats_/,
     },
     async execute(interaction, client) {
+        await interaction.deferUpdate();
         const userSettings = await client.getMongoUserData(interaction.user);
         const locale = userSettings.preferredLocale || interaction.locale;
-        await interaction.deferUpdate();
         const customId = interaction.customId;
+        const translations = {
+            embed: {
+                title: await client.translate(locale, 'commands', 'manga.response.stats.title'),
+                description: await client.translate(locale, 'commands', 'manga.response.stats.description'),
+                fields: {
+                    average: await client.translate(locale, 'commands', 'manga.response.stats.fields.average.name'),
+                    bayesian: await client.translate(locale, 'commands', 'manga.response.stats.fields.bayesian.name'),
+                    follows: await client.translate(locale, 'commands', 'manga.response.stats.fields.follows.name'),
+                    distribution: await client.translate(locale, 'commands', 'manga.response.stats.fields.distribution.name'),
+                    comments: await client.translate(locale, 'commands', 'manga.response.stats.fields.comments.name')
+                },
+                units: {
+                    votes: await client.translate(locale, 'commands', 'manga.response.stats.units.votes')
+                },
+                footer: await client.translate(locale, 'commands', 'manga.response.footer', { commandName: `/${interaction.message.interaction.commandName}`, user: interaction.user.username })
+            },
+            error: {
+                title: await client.translate(locale, 'commands', 'manga.response.error.title'),
+                description: await client.translate(locale, 'commands', 'manga.response.error.description.api')
+            },
+            buttons: {
+                forum: {
+                    open: await client.translate(locale, 'commands', 'manga.response.stats.buttons.forum.open'),
+                    no_thread: await client.translate(locale, 'commands', 'manga.response.stats.buttons.forum.no_thread')
+                }
+            }
+        }
 
         const match = customId.match(/^mangadex_stats_(.+)$/);
         if (!match) return null; // This should never happen even if MangaDex changes their ID format (famous last words, I know)
@@ -20,9 +47,9 @@ module.exports = {
         // Check if the API returned an error
         if (stats.result === 'error') {
             const embed = new EmbedBuilder()
-                .setTitle(await client.translate(locale, 'commands', 'manga.response.error.title'))
-                .setDescription(await client.translate(locale, 'commands', 'manga.response.error.description.api', { statusCode: `${stats.errors[0].status} - ${stats.errors[0].title}` }))
-                .setFooter({ text: await client.translate(locale, 'commands', 'manga.response.footer', { commandName: `/${interaction.message.interaction.commandName}`, user: interaction.user.username }), iconURL: client.user.avatarURL({ dynamic: true })})
+                .setTitle(translations.error.title)
+                .setDescription(translations.error.description)
+                .setFooter({ text: translations.embed.footer, iconURL: client.user.avatarURL({ dynamic: true })})
                 .setColor(Colors.Red);
             return interaction.followUp({ embeds: [embed], ephemeral: true });
         }
@@ -40,53 +67,30 @@ module.exports = {
 
         // Create an embed with the statistics data
         const embed = new EmbedBuilder()
-            .setTitle(await client.translate(locale, 'commands', 'manga.response.stats.title'))
-            .setDescription(await client.translate(locale, 'commands', 'manga.response.stats.description', { mangaId: title }))
+            .setTitle(translations.embed.title)
+            .setDescription(translations.embed.description.replace('%mangaId%', title))
             .addFields(
-                {   // Average rating
-                    name: await client.translate(locale, 'commands', 'manga.response.stats.fields[0].name'),
-                    value: `${averageRating}/10.00 (${totalVotes} ${await client.translate(locale, 'commands', 'manga.response.stats.units.votes')})`,
-                    inline: true
-                },
-                {   // Bayesian rating
-                    name: await client.translate(locale, 'commands', 'manga.response.stats.fields[1].name'),
-                    value: `${bayesianRating}/10.00`,
-                    inline: true
-                },
-                {   // Follows
-                    name: await client.translate(locale, 'commands', 'manga.response.stats.fields[2].name'),
-                    value: `${follows}`,
-                    inline: true
-                },
-                {   // Rating distribution
-                    name: await client.translate(locale, 'commands', 'manga.response.stats.fields[3].name'),
-                    value: distributionString,
-                    inline: true
-                },
-                {   // Comments
-                    name: await client.translate(locale, 'commands', 'manga.response.stats.fields[4].name'),
-                    value: `${statsData.comments?.repliesCount || 0}`,
-                    inline: true
-                }
+                { name: translations.embed.fields.average, value: `${averageRating}/10.00 (${totalVotes} ${translations.embed.units.votes})`, inline: true },
+                { name: translations.embed.fields.bayesian, value: `${bayesianRating}/10.00`, inline: true },
+                { name: translations.embed.fields.follows, value: `${follows}`, inline: true },
+                { name: translations.embed.fields.distribution, value: distributionString, inline: true },
+                { name: translations.embed.fields.comments, value: `${statsData.comments?.repliesCount || 0}`, inline: true }
             )
             .setColor(Colors.Blurple)
-            .setFooter({
-                text: await client.translate(locale, 'commands', 'manga.response.footer', { commandName: `/${interaction.message.interaction.commandName}`, user: interaction.user.username }),
-                iconURL: client.user.avatarURL({ dynamic: true })
-            });
+            .setFooter({ text: translations.embed.footer, iconURL: client.user.avatarURL({ dynamic: true }) });
 
             const buttons = new ActionRowBuilder();
             if (statsData.comments?.threadId) {
                 buttons.addComponents(
                     new ButtonBuilder()
-                        .setLabel(await client.translate(locale, 'commands', 'manga.response.stats.buttons.forum.open'))
+                        .setLabel(translations.buttons.forum.open)
                         .setURL(`https://forums.mangadex.org/threads/${statsData.comments.threadId}`)
                         .setStyle(ButtonStyle.Link)
                 );
             } else {
                 buttons.addComponents(
                     new ButtonBuilder()
-                    .setLabel(await client.translate(locale, 'commands', 'manga.response.stats.buttons.forum.no_thread'))
+                    .setLabel(translations.buttons.forum.no_thread)
                     .setURL('https://forums.mangadex.org/')
                     .setStyle(ButtonStyle.Link)
                     .setDisabled(true)

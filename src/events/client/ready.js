5f84755c319e67b2721051b71ed178b994b478f6
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2025-03-25 22:56:41 +0100

    Fix: Refactor logging to remove unnecessary default property access on chalk

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -7,18 +7,18 @@ module.exports = {
     async execute(client) {
         setInterval(client.pickPresence, 10 * 1000)
         const chalk = await getChalk();
-        console.log(chalk.default.greenBright(`[Discord] Ready as ${client.user.tag}`));
-        console.log(chalk.default.greenBright(`[Discord] Version: ${client.version}`));
+        console.log(chalk.greenBright(`[Discord] Ready as ${client.user.tag}`));
+        console.log(chalk.greenBright(`[Discord] Version: ${client.version}`));
 
         // Check for updates
-        console.log(chalk.default.blueBright('[GitHub] Checking for updates...'));
+        console.log(chalk.blueBright('[GitHub] Checking for updates...'));
         const res = await checkUpdates();
 
         if (res.isOutdated) {
-            console.warn(chalk.default.yellowBright(`[GitHub] The bot is outdated! Current version: ${client.version}, Latest version: ${res.latestVersion}`));
-            console.log(chalk.default.yellowBright(`[GitHub] Download the latest version at https://github.com/Bartolumiu/dexchan_rework/releases/latest`));
+            console.warn(chalk.yellowBright(`[GitHub] The bot is outdated! Current version: ${client.version}, Latest version: ${res.latestVersion}`));
+            console.log(chalk.yellowBright(`[GitHub] Download the latest version at https://github.com/Bartolumiu/dexchan_rework/releases/latest`));
         } else if (res.isOutdated === false) {
-            console.log(chalk.default.greenBright('[GitHub] The bot is up-to-date!'));
+            console.log(chalk.greenBright('[GitHub] The bot is up-to-date!'));
         } // If res.isOutdated is null, the error message will be logged in the checkUpdates function itself, so no need to log it here
     }
 }

1d10d16f149211e1e733d8c3e9ff4854620bf3f8
Bartolumiu <42890541+Bartolumiu@users.noreply.github.com>
2025-03-26 23:48:05 +0100

    Add JSDoc comments for getTitleCreators, getMangaDexCreators, and getNamiComiCreators functions for improved documentation

--- src/functions/titles/titleCreators.js
+++ src/functions/titles/titleCreators.js
@@ -1,3 +1,11 @@
+/**
+ * Returns a comma-separated string of title creators based on the provided type.
+ *
+ * @param {Object} title - The title object containing relationships.
+ * @param {string} type - The type of title. Valid values are 'mangadex' and 'namicomi'.
+ * @returns {string} A comma-separated list of creators.
+ * @throws {Error} Throws an error if the type is unsupported.
+ */
 const getTitleCreators = (title, type) => {
     switch (type) {
         case 'mangadex':
@@ -9,11 +17,26 @@ const getTitleCreators = (title, type) => {
     }
 }
 
+/**
+ * Extracts and returns a comma-separated string of unique creators (authors and artists)
+ * from the provided title object for the 'mangadex' type.
+ *
+ * @param {Object} title - The title object containing relationships.
+ * @returns {string} A comma-separated list of authors and artists.
+ */
 const getMangaDexCreators = (title) => {
     const creatorsAndArtists = Array.from(new Set(title.relationships.filter(rel => rel.type === 'author' || rel.type === 'artist').map(rel => rel.attributes.name))).join(', ');
     return creatorsAndArtists;
 }
 
+/**
+ * Extracts and returns a comma-separated string of unique organizations
+ * from the provided title object for the 'namicomi' type.
+ * In reality, there's only one organization per title, but this function is designed to handle multiple (just in case something changes in the future).
+ *
+ * @param {Object} title - The title object containing relationships.
+ * @returns {string} A comma-separated list of organizations.
+ */
 const getNamiComiCreators = (title) => {
     const creators = Array.from(new Set(title.relationships.filter(rel => rel.type === 'organization').map(rel => rel.attributes.name))).join(', ');
     return creators;

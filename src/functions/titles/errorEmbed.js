12a19e0d481461d81917a47e383de28563eca395
Bartolumiu <bartolumiu@tr25.es>
2025-07-26 02:11:08 +0200

    fix(errorEmbed): handle null embed case in sendErrorEmbed function

--- src/functions/titles/errorEmbed.js
+++ src/functions/titles/errorEmbed.js
@@ -1,6 +1,15 @@
 const { Colors, InteractionType } = require("discord.js");
 
+/**
+ * Sends an error embed in response to an interaction.
+ * @param {import("discord.js").Interaction} interaction - The interaction object from Discord.js.
+ * @param {Object} translations - The translations object containing localized strings.
+ * @param {import("discord.js").EmbedBuilder} embed - The embed to be sent.
+ * @param {string} errorKey - The key for the specific error message to be displayed.
+ * @returns {Promise<void>} A promise that resolves when the embed is sent.
+ */
 const sendErrorEmbed = (interaction, translations, embed, errorKey) => {
+    if (!embed) return;
     embed.setTitle(translations.embed.error.title)
         .setDescription(translations.embed.error.description[errorKey])
         .setColor(Colors.Red);

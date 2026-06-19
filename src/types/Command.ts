7f8d26324e01dcabc4819a5bea6a4bb62ce1e474
Bartolumiu <bartolumiu@tr25.es>
2026-06-20 00:19:21 +0200

    feat(rework): slash command interface

---
+++ src/types/Command.ts
@@ -0,0 +1,22 @@
+import {
+  SlashCommandBuilder,
+  ChatInputCommandInteraction,
+  AutocompleteInteraction,
+} from "discord.js";
+import { ExtendedClient } from "../lib/ExtendedClient";
+
+export interface SlashCommand {
+  global?: boolean;
+  data:
+    | SlashCommandBuilder
+    | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">
+    | any; // TODO: Remove after yeeting async data() from current commands
+  execute: (
+    interaction: ChatInputCommandInteraction,
+    client: ExtendedClient
+  ) => Promise<void>;
+  autocomplete?: (
+    interaction: AutocompleteInteraction,
+    client: ExtendedClient
+  ) => Promise<void>;
+}

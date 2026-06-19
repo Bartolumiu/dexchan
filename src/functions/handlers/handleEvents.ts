8f29f3ddb68c5e246f4f57fd851f96dfc7a042d1
Bartolumiu <bartolumiu@tr25.es>
2026-06-20 00:18:13 +0200

    feat(rework): event handler

---
+++ src/functions/handlers/handleEvents.ts
@@ -0,0 +1,48 @@
+import { readdirSync } from "node:fs";
+import { join } from "node:path";
+import { ExtendedClient } from "../../lib/ExtendedClient";
+import { BotEvent } from "../../types/Event";
+
+export default async function handleEvents(
+  client: ExtendedClient
+): Promise<void> {
+  const getChalk = require("../tools/getChalk");
+  const chalk = await getChalk();
+
+  console.log(chalk.blueBright("[Event Loader] Loading events..."));
+
+  const eventsPath = join(__dirname, "../../events");
+  const eventFolders = readdirSync(eventsPath);
+
+  for (const folder of eventFolders) {
+    if (folder === "mongo") continue; // Skip legacy mongo folder (deprecated)
+
+    const folderPath = join(eventsPath, folder);
+    const eventFiles = readdirSync(folderPath).filter(
+      (file) => file.endsWith(".ts") || file.endsWith(".js")
+    ); // TODO: Remove JS support
+
+    for (const file of eventFiles) {
+      const filePath = join(folderPath, file);
+      const eventModule = await import(filePath);
+      const event: BotEvent<any> = eventModule.default || eventModule;
+
+      if (event.name && typeof event.execute === "function") {
+        if (event.once) {
+          client.once(event.name, (...args) => event.execute(client, ...args));
+        } else {
+          client.on(event.name, (...args) => event.execute(client, ...args));
+        }
+        console.log(
+          chalk.greenBright(`[Event Loader] Loaded ${event.name} event.`)
+        );
+      } else {
+        console.error(
+          chalk.redBright(
+            `[Event Loader] Error: ${file} is missing a required 'name' or 'execute' property.`
+          )
+        );
+      }
+    }
+  }
+}

cef7bb2a24092db6b8201394c4a8189e9af8fd22
Bartolumiu <bartolumiu@tr25.es>
2026-07-14 00:51:58 +0200

    fix(sonar): ts:S6571

--- src/types/Component.ts
+++ src/types/Component.ts
@@ -6,35 +6,40 @@ import {
 } from "discord.js";
 import { ExtendedClient } from "../lib/ExtendedClient";
 
-export interface ExecutableItem {
-  execute?: (interaction: any, client: ExtendedClient) => Promise<void | any>;
+export interface ExecutableItem<TInteraction = unknown> {
+  execute?: (
+    interaction: TInteraction,
+    client: ExtendedClient
+  ) => Promise<unknown>;
   autocomplete?: (
     interaction: AutocompleteInteraction,
     client: ExtendedClient
-  ) => Promise<void | any>;
+  ) => Promise<unknown>;
 }
 
-export interface Component extends ExecutableItem {
+export interface Component<
+  TInteraction = unknown,
+> extends ExecutableItem<TInteraction> {
   data: {
     customId: string | RegExp;
   };
 }
 
-export interface ButtonComponent extends Component {
+export interface ButtonComponent extends Component<ButtonInteraction> {
   execute: (
     interaction: ButtonInteraction,
     client: ExtendedClient
   ) => Promise<void | any>;
 }
 
-export interface SelectMenuComponent extends Component {
+export interface SelectMenuComponent extends Component<AnySelectMenuInteraction> {
   execute: (
     interaction: AnySelectMenuInteraction,
     client: ExtendedClient
   ) => Promise<void | any>;
 }
 
-export interface ModalComponent extends Component {
+export interface ModalComponent extends Component<ModalSubmitInteraction> {
   execute: (
     interaction: ModalSubmitInteraction,
     client: ExtendedClient

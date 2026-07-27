a468a655bfe2bf4789a9bd7fd25d47edd101dda6
Bartolumiu <bartolumiu@tr25.es>
2026-07-27 03:20:54 +0200

    fix: remove deprecated 'commandErrors.api' special case

--- src/functions/titles/errorEmbed.ts
+++ src/functions/titles/errorEmbed.ts
@@ -11,9 +11,7 @@ export const sendErrorEmbed = async (
 ): Promise<void> => {
   if (!embed) return;
 
-  let description: string = interaction.isMessageComponent()
-    ? commandErrors.api
-    : commandErrors[errorKey];
+  let description: string = commandErrors[errorKey];
 
   if (!description) return;
 
